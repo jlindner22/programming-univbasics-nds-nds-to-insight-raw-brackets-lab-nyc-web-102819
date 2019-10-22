f6fa1252fd18e21f9724d93fa0095237f91d5966
Jen Lindner <jenlindner22@gmail.com>
2019-10-22 00:18:06 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -12,11 +12,11 @@ while director < directors.length do
 movie_index = 0
 while movie_index < movies.length do
   movies[movie_index][:worldwide_gross]
-      grand_total +=directors_database[info_index][:worldwide_gross]
-end
-  info_index += 1
+      grand_total +=directors_database[movie_index][:worldwide_gross]
+  movie_index += 1
 end
   director_index += 1
+end
 result
 end  
   
